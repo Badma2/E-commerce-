bc051d7cdb56a69ff78f95c95c07c67b3f350f62
Badma2 <badmaarag1025@gmail.com>
2022-11-22 16:43:30 +0800

    shoe

---
+++ src/data/Products.js
@@ -0,0 +1,74 @@
+const products = [
+  {
+    _id: "1",
+    name: "Velcro Ballerinas For Girl  (Pink)",
+    image:
+      "http://res.cloudinary.com/zpune/image/upload/v1644396418/random/11_dz0un.png",
+    description:
+      "Lorem Ipsut is simply dummy text of the printing and typesetting industry.",
+    price: 89,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    _id: "2",
+    name: "Velcro Sneakers For Boys & Girls  (Blue)",
+    image:
+      "http://res.cloudinary.com/zpune/image/upload/v1644396418/random/1_fiq56t.png",
+    description:
+      "Lorem Ipsut is simply dummy text of the printing and typesetting industry.",
+    price: 599,
+    countInStock: 10,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    _id: "3",
+    name: "Sesame Street Unisex-Child ELMO Puppet Slipper",
+    image:
+      "http://res.cloudinary.com/zpune/image/upload/v1644396419/random/10_kalats.png",
+    description:
+      "Lorem Ipsut is simply dummy text of the printing and typesetting industry.",
+    price: 929,
+    countInStock: 0,
+    rating: 3.5,
+    numReviews: 3,
+  },
+  {
+    _id: "4",
+    name: "Lace Casual Boots For Boys & Girls  (Tan)",
+    image:
+      "http://res.cloudinary.com/zpune/image/upload/v1644396427/random/10_kalats.png",
+    description:
+      "Lorem Ipsut is simply dummy text of the printing and typesetting industry.",
+    price: 399,
+    countInStock: 0,
+    rating: 3.5,
+    numReviews: 3,
+  },
+  {
+    _id: "5",
+    name: "Sesame Street Unisex-Child ELMO Puppet Slipper",
+    image:
+      "http://res.cloudinary.com/zpune/image/upload/v1644396419/random/10_kalats.png",
+    description:
+      "Lorem Ipsut is simply dummy text of the printing and typesetting industry.",
+    price: 929,
+    countInStock: 0,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    _id: "6",
+    name: "Women Red Heels Sandal",
+    image:
+      "http://res.cloudinary.com/zpune/image/upload/v1644396419/random/10_kalats.png",
+    description:
+      "Lorem Ipsut is simply dummy text of the printing and typesetting industry.",
+    price: 29,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+];
