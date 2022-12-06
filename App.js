6b5879593e4145ffe1d708b4adcd4162aaca33ea
Badma2 <badmaarag1025@gmail.com>
2022-12-06 15:40:24 +0800

    shop

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { Box, NativeBaseProvider, Text } from "native-base";
-import HomeScreen from "./src/Screens/HomeScreen";
+import LoginScreen from "./src/Screens/LoginScreen";
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
-        <Text>Open up App.js to start working on your app!</Text>
-      </Box>
+      <LoginScreen />
     </NativeBaseProvider>
   );
 }
--- src/Screens/LoginScreen.js
+++ src/Screens/LoginScreen.js
@@ -1,11 +1,43 @@
-import { Text, View } from "native-base";
+import { Box, Heading, Image, Text, View } from "native-base";
 import React from "react";
+import Colors from "../color";
+import { MaterialIcons } from "@expo/vector-icons";
 
 function LoginScreen() {
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
+    <Box flex={1} bg={Colors.black}>
+      <Image
+        flex={1}
+        alt="Logo"
+        resizeMode="cover"
+        size="lg"
+        w="full"
+        source={require("../../assets/cover.png")}
+      />
+      <Box
+        w="full"
+        h="full"
+        position="absolute"
+        top="0"
+        px="6"
+        justifyContent="center"
+      >
+        <Heading>LOGIN</Heading>
+        <VStack space={5} pt="6">
+          <Input
+            InputLeftElement={
+              <MaterialIcons name="email" size={20} color={Colors.main} />
+            }
+            variant="underlined"
+            placeholder="user@gmail.com"
+            w="70%"
+            pl={2}
+            color={Colors.main}
+            borderBottomColor={Colors.underline}
+          />
+        </VStack>
+      </Box>
+    </Box>
   );
 }
 
